08da86cb2e53f4cd97d054643ee2a19088adec3a
Joao <joaopedroplacidodesouza@gmail.com>
2024-05-09 21:16:34 -0300

    commit

---
+++ arquivo.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMCOntentLoaded', function() { 
+    alert(123);
+    //let = posso alterar valor (para contas é perfeito)
+    //const = não posso alterar o valor 
+//tipos de dados
+//string: Sequencia de caractere
+//number: numeros inteiros e pontos flutuantes
+//object: estrutura de dados que armazena multiplos valores
+//boolean: retorna dois valores true e false
+//null: quando esta vazio
+//array: objeto especial ou ordenado 
+//function: bloco de código reutilizavel
+})
