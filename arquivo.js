e776bf7a9c83b9213ef8356033c9ff93d0eae3c7
Joao <joaopedroplacidodesouza@gmail.com>
2024-05-16 21:29:32 -0300

    iniciando projeto

--- arquivo.js
+++ arquivo.js
@@ -26,7 +26,10 @@ alert("aluno quase passou")
 != se é diferente
 !== se é estritamente diferente
 */
-
+/*
+document.addEventListener('DOMContentLoaded', function() { 
+    window.alert(123); 
+}); */
     //let = posso alterar valor (para contas é perfeito)
     //const = não posso alterar o valor 
 //tipos de dados
@@ -43,11 +46,52 @@ alert("aluno quase passou")
 
 //Funções são blocos de código que podem ser chamados e 
 //reutilizados :)
+//Dentro dos () funções, vão parametros
+//Posso passar qualquer coisa como parametro, variaveis, arrays e etc
+
 
 function PrimeiraFunção () { 
-    return 'Olá, essa é a disciplina de desenvolvimento web';
+    return `${parametro}, ${segundoParametro}`;
 }
-PrimeiraFunção();
+const segundoParametro = ", agora com dois parametros"
+const parametro = "Essa é uma função com parametro"
+console.log(PrimeiraFunção(parametro, segundoParametro));
+//--------------------------------- / ---------------------
+//arrays em JavaScript são estrutura de dados que permitem
+//armazenar uma coleção ordenada de elementos.
 
+let joao = [
+    idade = 18,
+    altura = 1.73,
+    cidade = "francisco beltrao",
+    animal = false
+]
 
-});
+console.log(joao[0]);
+console.log(joao[1]);
+
+//-------------------------------- / --------------
+//Os objetos são coleções NÃO ORDENADAS de 
+//pares chave-valor, onde cada valor é associado
+//quando utiliza-se { } se refere a objeto ao invés de array
+//define manualmente as chaves
+
+let pessoa = { 
+nome: 'Maria',
+time: "corinthians",
+ptofissao: 'vendedora',
+idadae: 18
+
+}
+
+console.log(pessoa["curso"]); //para chamar uma "Variavel" pelo objeto devo colocar o nome dela entre aspas " "
+
+//-------------------------- / -------------
+
+//ForEach faz a leitura de todos os valores dentro de um vetor
+const numeros = ["A", "B", "C", "D"]
+
+numeros.forEach(num => {
+    console.log(num)
+})
+
